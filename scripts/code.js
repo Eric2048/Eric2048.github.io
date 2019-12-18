a7cf1da1b801f8c6a3b4876b0c2169f226a92f59
Eric E. Del Sesto <eric.delsesto@gmail.com>
2019-12-18 01:04:07 -0800

    Tweaks
    Switched to 2-char tabs. Fixed unmatched Divs. Changed back to full-page scrolling.  Fixed the scroll to top button.

--- scripts/code.js
+++ scripts/code.js
@@ -1,26 +1,22 @@
 // This code runs when the page is first loaded.
-console.log("running");
 
-// Find the div.scrollingMiddle
-var o = document.getElementById("scrollingMiddle");
-if (o) {
-  // When it scrolls, call this function.
-  o.onscroll = function() {
-    // Find the back-to-top button
-    var b = document.getElementById("backToTopButton");
-    if (b) {
-      // If the DIV is scrolled to the top, hide this button, otherwise show it.
-      if (o.scrollTop === 0) {
-        b.style.display = "none";
-      } else {
-        b.style.display = "block";        
-      }
-    }    
-  };
-}
+// When page scrolls, call this function.
+document.body.onscroll = function() {
+  // Find the back-to-top button
+  var b = document.getElementById("backToTopButton");
+  if (b) {
+    // If the DIV is scrolled to the top, hide this button, otherwise show it.
+    if (window.scrollY === 0) {
+      b.style.display = "none";
+    } else {
+      console.log("showing");
+      b.style.display = "block";
+    }
+  }
+};
 
 // The back-to-top button calls this when it is clicked.
 function scrollToTop() {
-  o.scrollTop = 0;
+  document.body.scrollTop = document.documentElement.scrollTop = 0;
 }
 
